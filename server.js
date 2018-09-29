8407d7fb34457871134594b6739b91ba9aec800d
Scott <Soltis13@gmail.com>
2018-09-29 16:42:28 -0500

    udpate heroku build and server port settings

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -33,6 +29,17 @@ app.use(passport.initialize());
 
 // Use Routes
 app.use('/api/articles', articles);
+
+//Server static assets if in production
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  }) 
+}
 
 
 
