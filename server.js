4b2941c7d09d35c5e7bd28ff5245cfc70795297b
Scott <soltis13@gmail.com>
2018-10-02 18:39:46 -0500

    removed production server static call

--- server.js
+++ server.js
@@ -31,14 +31,14 @@ app.use(passport.initialize());
 app.use('/api/articles', articles);
 
 //Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
 
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  }) 
-}
+//set static folder
+app.use(express.static("client/build"));
+
+app.get('*', (req,res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+}) 
+
 
 
 
