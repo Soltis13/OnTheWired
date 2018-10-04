3c8967aea232c2f994dadef0e082d6fb83c95997
Scott <Soltis13@gmail.com>
2018-10-03 19:48:26 -0500

    update package

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -33,6 +33,7 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  
   update: function(req, res) {
     db.Article
       .findOneAndUpdate({ _id: req.params.id }, req.body)
--- server.js
+++ server.js
@@ -41,6 +41,4 @@ if (process.env.NODE_ENV === "production") {
   }) 
 }
 
-
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
