a352d5dec4ccaa9e81cb71a0144dd89ba13ff4d6
Scott Soltis <36977170+Soltis13@users.noreply.github.com>
2018-10-13 12:41:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -35,9 +35,6 @@ app.use('/api/articles', articles);
 //set static folder
 app.use(express.static("client/build"));
 
-app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-}) 
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
