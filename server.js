0ff1bee25bd0846b97db1cef614c4a827ae20a0c
Scott <soltis13@gmail.com>
2018-10-15 10:31:53 -0500

    updated server

--- server.js
+++ server.js
@@ -32,9 +32,16 @@ app.use('/api/articles', articles);
 
 //Server static assets if in production
 
-//set static folder
-app.use(express.static("client/build"));
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  }
 
-
+  else {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
